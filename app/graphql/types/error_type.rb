0665f7e03325a9d976015d46af8f201f08917f1a
Uduak Essien <acushla4real@gmail.com>
2022-03-20 20:53:38 +0100

    Add error type

---
+++ app/graphql/types/error_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class ErrorType < Types::BaseObject
+
+    description "ActiveRecord errors"
+
+    field :field_name, String, null: false
+    field :errors, String, null: false
+  end
+end
